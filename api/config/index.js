69498d998f4eeeecbcb3a32656f2a9353cbac1c9
Muhammet Ali <m.ali.maksadow@gmail.com>
2024-12-01 10:59:15 +0300

    JWT hackleme onlemi alindi (jwt secret guncellendi)

--- api/config/index.js
+++ api/config/index.js
@@ -3,7 +3,7 @@ module.exports = {
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
     "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://localhost:27017/nodejs",
     "JWT": {
-        "SECRET": "123435",
+        "SECRET": process.env.JWT_SECRET || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eJmYTEyZDIzYjc0ZDA5NSIsImV4cCI6MTczMzEyMzMzMH0.s7z7OpfQZZW40ygeOGMcerzh7IFHi3v1tI5ICwhm-zkyJpZCI6IjY3MjRjNDdlOWzYjc0ZDA5NSIsImV4cCI6MTczMzEyMzMzMH0.s7z7OI5ICwhm-zk",
         "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 // 86400
     },
     "DEFAULT_LANG": process.env.DEFAULT_LANG || "EN",
